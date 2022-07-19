a09ef018da3080e21e7b315452b18625459d66c7
niecong <niecong@cvter.localdomain>
2022-07-19 15:05:01 +0800

    changes: support to update system time whenever system time changed.

---
+++ scheduler/scheduler/time_utils.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "croncpp.h"
+#include <iostream>
+
+#define LOG_DEBUG std::cout << PrintTime()
+
+static std::string Time()
+{
+    std::time_t t = std::time(0);   // get time now
+    std::tm* now = std::localtime(&t);
+
+    return "[" +  cron::utils::to_string(*now) + "]";
+}
+
+static std::string TimeTotm(int64_t tick)
+{
+    using namespace std::chrono_literals;
+    auto duration = std::chrono::system_clock::duration(tick);
+
+    auto tp = std::chrono::system_clock::time_point(duration);
+    auto time = std::chrono::system_clock::to_time_t(tp);
+    tm tm;
+    cron::utils::time_to_tm(&time, &tm);
+    return cron::utils::to_string(tm);
+}
